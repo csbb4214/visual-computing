48a43aeab5b854f74ef3c460d69a24ae509ebf5e
csbb4214 <csbb4214@student.uibk.ac.at>
2025-11-27 14:36:53 +0100

    task 1

--- Assignment_4/src/ground.cpp
+++ Assignment_4/src/ground.cpp
@@ -53,7 +53,37 @@ Ground groundCreate(const std::string& filePath) {
             float displacement = groundDisplacement(ground.waveParamsVec, {x, z});
             vertexData[i * 8 + 1] += displacement;
             
-            // TODO: update normals
+            /* Update normals */ 
+            // Calculate partial derivatives for each wave
+            float dh_dx = 0.0f;
+            float dh_dz = 0.0f;
+
+            for (WaveParams waveParams : ground.waveParamsVec) {
+                Vector2D normalizedDir = normalize(waveParams.direction);
+                float dotProduct = dot(normalizedDir, {x, z});
+    
+                // Partial derivative with respect to x
+                dh_dx += waveParams.amplitude * waveParams.omega * normalizedDir.x * 
+                        static_cast<float>(cos(waveParams.omega * dotProduct));
+    
+                // Partial derivative with respect to z (=y in the mathematical notation)
+                dh_dz += waveParams.amplitude * waveParams.omega * normalizedDir.y * 
+                        static_cast<float>(cos(waveParams.omega * dotProduct));
+            }
+
+            // Computed normal using cross product: n = -(1, dh/dx, 0) x (0, dh/dz, 1)
+            Vector3D normal = {
+                -dh_dx,
+                1.0f,
+                -dh_dz
+            };
+
+            // Write the normalized normal to the buffer
+            normal = normalize(normal);
+
+            vertexData[i * 8 + 3] = normal.x;
+            vertexData[i * 8 + 4] = normal.y;
+            vertexData[i * 8 + 5] = normal.z;
         }
 
         glUnmapBuffer(GL_ARRAY_BUFFER);
